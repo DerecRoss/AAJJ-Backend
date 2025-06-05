4bb61522f45540415891d79cd1ebf4055dbab76b
DerecRoss <derecdz999@gmail.com>
2025-06-05 12:52:13 -0300

    setup: initializing project

---
+++ src/main/java/br/com/aajj/ajj_backend/AjjBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.aajj.ajj_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjjBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjjBackendApplication.class, args);
+	}
+
+}
