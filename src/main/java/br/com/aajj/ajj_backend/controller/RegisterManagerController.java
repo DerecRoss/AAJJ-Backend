c961fe8679d6ccb7851899a379c4ea97395da47a
DerecRoss <derecdz999@gmail.com>
2025-06-08 10:29:24 -0300

    feat: signup system with validation has been completed

--- src/main/java/br/com/aajj/ajj_backend/controller/RegisterManagerController.java
+++ src/main/java/br/com/aajj/ajj_backend/controller/RegisterManagerController.java
@@ -3,6 +3,8 @@ package br.com.aajj.ajj_backend.controller;
 import br.com.aajj.ajj_backend.domain.User;
 import br.com.aajj.ajj_backend.dto.UserDto;
 import br.com.aajj.ajj_backend.service.UserService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +15,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/register")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class RegisterManagerController {
 
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> save(@RequestBody UserDto userDto){
+    public ResponseEntity<User> save(@RequestBody @Valid UserDto userDto){
         User save = userService.save(userDto);
-        return new ResponseEntity<>(save, HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(save, HttpStatus.CREATED);
     }
 
     @GetMapping(path = "/find")
--- src/main/java/br/com/aajj/ajj_backend/dto/UserDto.java
+++ src/main/java/br/com/aajj/ajj_backend/dto/UserDto.java
@@ -3,6 +3,7 @@ package br.com.aajj.ajj_backend.dto;
 import br.com.aajj.ajj_backend.domain.Charge;
 import br.com.aajj.ajj_backend.domain.Teacher;
 import br.com.aajj.ajj_backend.domain.Time;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,12 +12,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
-    private Integer id;
+
+    @NotEmpty(message = "User name can't be null")
     private String name;
+
+    @NotEmpty(message = "User name can't be null")
     private String password;
+
+    @NotEmpty(message = "User name can't be null")
     private String email;
+
+    @NotEmpty(message = "User name can't be null")
     private String phone;
+
     private Teacher teacher;
+
     private Charge charge;
+
     private Time time;
 }
