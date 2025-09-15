b2d0db3c43dfe73ef3124af68173316a2841c795
DerecRoss <derecdz999@gmail.com>
2025-09-15 13:00:45 -0300

    bug fix: controller for images

---
+++ src/main/java/br/com/aajj/ajj_backend/controller/ImageController.java
@@ -0,0 +1,40 @@
+package br.com.aajj.ajj_backend.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Controller
+@RequestMapping("/files")
+public class ImageController {
+    private final Path directory = Paths.get("/home/derec/Documentos/aajj-web-service/ajj-backend/src/main/resources/static/images");
+
+    @GetMapping("/{filename}")
+    public ResponseEntity<Resource> getFile(@PathVariable String fileName){
+        try {
+            Path file = directory.resolve(fileName).normalize();
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() && resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.getFilename() + "\"")
+                        .contentType(MediaType.IMAGE_PNG)
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (MalformedURLException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
